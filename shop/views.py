ff1102265e1ae2a4aedcc104b8ac6ace8b4c0f74
Aurojyoti23 <aurojyotitusarkantidas@gmail.com>
2022-10-01 16:41:37 +0530

    Push to github

---
+++ shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4) - (n//4))
+    params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+    return render(request, 'shop/index.html', params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return HttpResponse("We are in contact section")
+
+def tracker(request):
+    return HttpResponse("We are in tracker section")
+
+def search(request):
+    return HttpResponse("We are in search section")
+
+def productview(request):
+    return HttpResponse("We are in productview section")
+
+def checkout(request):
+    return HttpResponse("We are in checout section")
+
